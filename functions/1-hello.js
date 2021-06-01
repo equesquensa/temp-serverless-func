4692e8292029e52ecc6b1423c507e013cacb7eeb
Eduardo Quesquen <equesquensa@gmail.com>
2021-06-01 07:50:08 -0500

    sev¿rrverles funct

---
+++ functions/1-hello.js
@@ -0,0 +1,10 @@
+//domain/.netlify/functions/1-hello
+
+// const person = { name: 'jhon' };
+
+exports.handler = async (event, context, cb) => {
+	return {
+		statusCode: 200,
+		body: 'Our First Netlify Function Example',
+	};
+};
