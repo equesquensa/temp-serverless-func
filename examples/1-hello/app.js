d78af0c55e4a4e58e3998dee984e5a9621cacf08
Eduardo Quesquen <equesquensa@gmail.com>
2021-06-01 08:02:01 -0500

    fixed example

--- examples/1-hello/app.js
+++ examples/1-hello/app.js
@@ -6,7 +6,7 @@ const fetchData = async () => {
 		const { data } = await axios.get('/api/1-hello');
 		result.textContent = data;
 	} catch (error) {
-		console.log(error.response);
+		// console.log(error.response);
 		result.textContent = error.response.data;
 	}
 };
