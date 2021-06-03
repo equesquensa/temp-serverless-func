39c7a606ef8f2860779fbe076eb56e487c7dd6a9
Eduardo Quesquen <equesquensa@gmail.com>
2021-06-03 06:35:05 -0500

    example 3 complete

---
+++ functions/3-product.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+
+const Airtable = require('airtable-node');
+
+const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
+	.base('appx7bn7tsRH4huZO')
+	.table('products');
+
+exports.handler = async (event, context, cb) => {
+	const { id } = event.queryStringParameters;
+	if (id) {
+		try {
+			const product = await airtable.retrieve(id);
+			if (product.error) {
+				return {
+					statusCode: 404,
+					body: `No product with id: ${id}`,
+				};
+			}
+			return {
+				headers: {
+					'Access-Control-Allow-Origin': '*',
+				},
+				statusCode: 200,
+				body: JSON.stringify(product),
+			};
+		} catch (error) {
+			return {
+				statusCode: 500,
+				body: `Server Error`,
+			};
+		}
+	}
+	return {
+		statusCode: 400,
+		body: 'Please provide a id',
+	};
+};
---
+++ functions/3-z-complete.js
@@ -0,0 +1,52 @@
+require('dotenv').config();
+
+const Airtable = require('airtable-node');
+
+const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
+	.base('appx7bn7tsRH4huZO')
+	.table('products');
+
+exports.handler = async (event, context, cb) => {
+	const { id } = event.queryStringParameters;
+	if (id) {
+		try {
+			const product = await airtable.retrieve(id);
+			if (product.error) {
+				return {
+					statusCode: 404,
+					body: `No product with id: ${id}`,
+				};
+			}
+			return {
+				headers: {
+					'Access-Control-Allow-Origin': '*',
+				},
+				statusCode: 200,
+				body: JSON.stringify(product),
+			};
+		} catch (error) {
+			return {
+				statusCode: 500,
+				body: `Server Error`,
+			};
+		}
+	}
+	try {
+		const { records } = await airtable.list();
+		const products = records.map((product) => {
+			const { id } = product;
+			const { name, image, price } = product.fields;
+			const url = image[0].url;
+			return { id, name, url, price };
+		});
+		return {
+			statusCode: 200,
+			body: JSON.stringify(products),
+		};
+	} catch (error) {
+		return {
+			statusCode: 500,
+			body: 'Server Error',
+		};
+	}
+};
